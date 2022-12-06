6ed34ac454e24ee855b31461647fb4ccb302aa26
Arnut Klamhui <kla.arnut@gmail.com>
2022-12-06 11:30:28 +0700

    update

--- userreport/userReport.py
+++ userreport/userReport.py
@@ -10,14 +10,18 @@ class userReport():
     name = 'userReport'
 
     def __init__(self):
+        #define config
         try:
             import configs
         except ImportError:
                 configs = {}
+
+        #set config
         self.secretKey = getattr(configs, 'secret_key', '')
         
-        self.headless = True
-        self.chromeArgs = ['--no-sandbox','start-maximized','disable-infobars','--disable-extensions','window-size=1024,768']
+        #chromedriver args
+        self.headless = False
+        self.chromeArgs = ['--no-sandbox','start-maximized','disable-infobars','--disable-extensions','window-size=1024,768','--disable-gpu']
         if self.headless == True: self.chromeArgs.append('--headless')
         
 
@@ -29,9 +33,8 @@ class userReport():
     def getpage(self):
         url = "https://scrapeme.live/shop/"
         options = webdriver.ChromeOptions()
-        options.headless = self.headless
         for arg in self.chromeArgs:
             options.add_argument(arg)
     
-        with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) as driver:
+        with webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options) as driver:
 	        driver.get(url)
